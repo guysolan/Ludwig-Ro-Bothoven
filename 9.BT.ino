c9bbad7084192d80b9e3a7c944db65c3556e3b55
Guy_Solan <56299900+guysolan@users.noreply.github.com>
2021-03-22 18:04:26 +0000

    Add files via upload

---
+++ 9.BT.ino
@@ -0,0 +1,326 @@
+#include <PS3BT.h>
+#include <usbhub.h>
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+USB Usb;
+BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
+PS3BT PS3(&Btd); // This will just create the instance
+
+bool printTemperature, printAngle;
+
+// Servo Initialisation
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+//DC Motor Initialisation
+
+#define ENABLE 4
+#define DIRA 2
+#define DIRB 3
+
+// 74HC595 Set Up
+int latchPin = 6; //Pin 12 of 74HC595
+int clockPin = 7; //Pin 11 of 74HC595
+int dataPin = 5; //Pin 14 of 74HC595
+int dt = 250;
+
+byte LED0s = 0b00000000;
+byte LED1s = 0b10000000;
+byte LED2s = 0b01000000;
+byte LED3s = 0b00100000;
+byte LED4s = 0b00010000;
+byte LED5s = 0b00001000;
+byte LED6s = 0b00000100;
+byte LED7s = 0b00000010;
+byte LED8s = 0b00000001;
+
+//byte 1LED[] = {0b10000000,
+//               0b01000000,
+//               0b00100000,
+//               0b00010000,
+//               0b00001000,
+//               0b00000100,
+//               0b00000010,
+//               0b00000001
+//              };
+
+byte REDLEDs = 0b10101010;
+byte BLUELEDs = 0b01010101;
+
+int bpm = 1000;
+int semi_demi_hemi_quaver = bpm / 16;
+int semi_quaver = bpm / 4;
+int quaver = bpm / 2;
+int dot_quaver = bpm * 3 / 4;
+int crotchet = bpm;
+int dot_crotchet = bpm * 3 / 2;
+int minim = bpm * 2;
+
+bool started = false;
+
+void setup() {
+  Serial.begin(115200);
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); //halt
+  }
+  Serial.print(F("\r\nPS3 Bluetooth Library Started"));
+
+  servo1.attach(13, 600, 2300);
+  servo2.attach(12, 600, 2300);
+  servo3.attach(11, 600, 2300);
+  servo4.attach(8, 600, 2300);
+  Serial.print(F("\r\nServos Attached"));
+
+  //---set pin direction for DC motor
+  pinMode(ENABLE, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  Serial.print(F("\r\nLEDs Attached"));
+}
+void loop() {
+
+  Usb.Task();
+
+  if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
+    if (PS3.getAnalogHat(LeftHatX) > 147 ||  PS3.getAnalogHat(LeftHatY) > 147 || PS3.getAnalogHat(RightHatX) > 137 || PS3.getAnalogHat(RightHatY) > 137) {
+      Serial.print(F("\r\nLeftHatX: "));
+      Serial.print(PS3.getAnalogHat(LeftHatX));
+
+      analogWrite(ENABLE, 255); //enable on
+      digitalWrite(DIRA, HIGH); //one way
+      digitalWrite(DIRB, LOW);
+      analogWrite(ENABLE, LeftHatX); //half speed
+
+      Serial.print(F("\tLeftHatY: "));
+      Serial.print(PS3.getAnalogHat(LeftHatY));
+      if (PS3.PS3Connected) { // The Navigation controller only have one joystick
+        Serial.print(F("\tRightHatX: "));
+        Serial.print(PS3.getAnalogHat(RightHatX));
+        Serial.print(F("\tRightHatY: "));
+        Serial.print(PS3.getAnalogHat(RightHatY));
+      }
+    }
+    else if (PS3.getAnalogHat(LeftHatX) < 117 || PS3.getAnalogHat(LeftHatY) < 117 ||  PS3.getAnalogHat(RightHatX) < 117 || PS3.getAnalogHat(RightHatY) < 117) {
+      Serial.print(F("\r\nLeftHatX: "));
+      Serial.print(PS3.getAnalogHat(LeftHatX));
+
+      analogWrite(ENABLE, 255); //enable on
+      digitalWrite(DIRB, LOW);
+      digitalWrite(DIRA, HIGH); //other way
+      analogWrite(ENABLE, LeftHatX); //half speed
+
+
+      Serial.print(F("\tLeftHatY: "));
+      Serial.print(PS3.getAnalogHat(LeftHatY));
+      if (PS3.PS3Connected) { // The Navigation controller only have one joystick
+        Serial.print(F("\tRightHatX: "));
+        Serial.print(PS3.getAnalogHat(RightHatX));
+        Serial.print(F("\tRightHatY: "));
+        Serial.print(PS3.getAnalogHat(RightHatY));
+      }
+    }
+    else {
+      digitalWrite(ENABLE, LOW); //all done
+    }
+
+    // Analog button values can be read from almost all buttons
+    if (PS3.getAnalogButton(L2) || PS3.getAnalogButton(R2)) {
+      Serial.print(F("\r\nL2: "));
+      Serial.print(PS3.getAnalogButton(L2));
+
+      //      analogWrite(ENABLE, 255); //enable on
+      //      digitalWrite(DIRA, HIGH); //one way
+      //      digitalWrite(DIRB, LOW);
+      //      analogWrite(ENABLE, PS3.getAnalogButton(L2)); //half speed
+
+      if (PS3.PS3Connected) {
+        Serial.print(F("\tR2: "));
+        Serial.print(PS3.getAnalogButton(R2));
+
+        //        analogWrite(ENABLE, 255); //enable on
+        //        digitalWrite(DIRB, LOW);
+        //        digitalWrite(DIRA, HIGH); //other way
+        //        analogWrite(ENABLE, LeftHatX); //half speed
+      }
+    }
+
+    if (PS3.getButtonClick(PS)) {
+      Serial.print(F("\r\nPS"));
+      PS3.disconnect();
+    }
+    else {
+      if (PS3.getButtonClick(SQUARE)) {
+        Serial.print(F("\r\nSquare"));
+        lightServo(LED8s, servo1, 0, 200);
+      }
+      if (PS3.getButtonClick(TRIANGLE)) {
+        Serial.print(F("\r\nTriangle"));
+        lightServo(LED7s, servo1, 180, 200);
+      }
+      if (PS3.getButtonClick(CIRCLE)) {
+        Serial.print(F("\r\nCircle"));
+        lightServo(LED6s, servo2, 0, 200);
+      }
+      if (PS3.getButtonClick(CROSS)) {
+        Serial.print(F("\r\nCross"));
+        lightServo(LED5s, servo2, 180, 200);
+      }
+      if (PS3.getButtonClick(UP)) {
+        Serial.print(F("\r\nUp"));
+        lightServo(LED4s, servo3, 0, 200);
+      }
+      if (PS3.getButtonClick(RIGHT)) {
+        Serial.print(F("\r\nRight"));
+        lightServo(LED3s, servo3, 180, 200);
+      }
+      if (PS3.getButtonClick(DOWN)) {
+        Serial.print(F("\r\nDown"));
+        lightServo(LED2s, servo4, 0, 200);
+      }
+      if (PS3.getButtonClick(LEFT)) {
+        Serial.print(F("\r\nLeft"));
+        lightServo(LED1s, servo4, 180, 200);
+      }
+
+      if (PS3.getButtonClick(L1)) {
+        Serial.print(F("\r\nL1"));
+        moveleft();
+      }
+
+      if (PS3.getButtonClick(R1)) {
+        Serial.print(F("\r\nR1"));
+        moveright();
+      }
+
+
+
+      if (PS3.getButtonClick(SELECT)) {
+        Serial.print(F("\r\nSelect - "));
+//        PS3.printStatusString();
+        seven_nation_army();
+      }
+      if (PS3.getButtonClick(START)) {
+        Serial.print(F("\r\nStart"));
+        //        printAngle = !printAngle;
+        sonata();
+      }
+    }
+  }
+}
+
+void lightServo(byte LEDN, Servo & theServo, int angle, int duration) {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LEDN);
+  digitalWrite(latchPin, HIGH);
+  theServo.write(angle);
+  delay(duration * 4 / 5);
+  theServo.write(90);
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED0s);
+  digitalWrite(latchPin, HIGH);
+  delay(duration * 1 / 5);
+}
+
+void moveleft() {
+  digitalWrite(ENABLE, HIGH); // enable on
+  digitalWrite(DIRA, HIGH); //one way
+  digitalWrite(DIRB, LOW);
+  delay(1000);
+  digitalWrite(ENABLE, LOW); // disable
+}
+
+void moveright() {
+  digitalWrite(ENABLE, HIGH);
+  digitalWrite(DIRA, LOW); //reverse
+  digitalWrite(DIRB, HIGH);
+  delay(1000);
+  digitalWrite(ENABLE, LOW); // disable
+}
+
+void lightup(byte LEDN, int duration) {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LEDN);
+  digitalWrite(latchPin, HIGH);
+  delay(duration * 4 / 5);
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LED0s);
+  digitalWrite(latchPin, HIGH);
+  delay(duration * 1 / 5);
+}
+
+void staylit(byte LEDN) {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, LEDN);
+  digitalWrite(latchPin, HIGH);
+}
+
+void rest(int duration) {
+  delay(duration);
+}
+
+void seven_nation_army() {
+  bpm = 700;
+  //Seven Nation Army
+  //Bar 1
+  lightServo(LED5s, servo2, 180, dot_crotchet);
+  lightServo(LED5s, servo2, 180, quaver);
+  lightServo(LED7s, servo1, 180, dot_quaver);
+  lightServo(LED5s, servo2, 180, semi_quaver);
+  rest(quaver);
+  lightServo(LED4s, servo3, 0, quaver);
+  //Bar 2
+  lightServo(LED3s, servo3, 180, minim);
+  lightServo(LED2s, servo4, 0, minim);
+}
+
+void sonata() {
+  //Bar 3
+  lightServo(LED2s, servo4, 0, semi_quaver); //F
+  lightServo(LED3s, servo3, 180, semi_quaver); //G
+  //BEAT
+  lightServo(LED4s, servo3, 0, semi_quaver); //A
+  lightServo(LED5s, servo2, 180, semi_quaver); //B
+  lightServo(LED6s, servo2, 0, semi_quaver); //C
+  lightServo(LED7s, servo1, 180, semi_quaver);//D
+  //BEAT
+  lightServo(LED8s, servo1, 0, semi_quaver); //E
+  lightServo(LED7s, servo1, 180, semi_quaver); //D
+  lightServo(LED6s, servo2, 0, semi_quaver); //C
+  lightServo(LED5s, servo2, 180, semi_quaver); //B
+  //BEAT
+  lightServo(LED4s, servo3, 0, semi_quaver); //A
+  lightServo(LED3s, servo3, 180, semi_quaver); //G
+  lightServo(LED2s, servo4, 0, semi_quaver); //F
+  lightServo(LED1s, servo4, 180, semi_quaver); //E
+  rest(crotchet);
+  //beat
+  rest(quaver);
+  rest(semi_quaver);
+  lightServo(LED1s, servo4, 180, semi_quaver); //C
+  // BEAT
+  lightServo(LED2s, servo4, 0, semi_quaver); //D
+  rest(quaver);
+  lightServo(LED1s, servo4, 180, semi_quaver); //C
+  //BEAT
+  lightServo(LED2s, servo4, 0, semi_quaver); //D
+  lightServo(LED3s, servo3, 180, semi_quaver); //E
+  lightServo(LED4s, servo3, 0, semi_quaver); //F
+  lightServo(LED5s, servo2, 180, semi_quaver); //G
+}
---
+++ B.ESP_Servos.ino
@@ -0,0 +1,204 @@
+#include <Ps3Controller.h>
+#include <ESP32_Servo.h>
+
+//#define ENABLE 18
+//#define DIRA 17
+//#define DIRB 5
+//
+#define ENABLE 27
+#define DIRA 26
+#define DIRB 14
+//
+#define motor1Pin1 27
+#define motor1Pin2  26
+#define enable1Pin 14
+
+// Setting PWM properties
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int dutyCycle = 200;
+
+//Digital Push Control
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+int servoTurnAngle = 90;
+
+void clockwise(Servo &theServo) {
+  theServo.write(90 - servoTurnAngle);
+}
+
+void anticlockwise(Servo &theServo) {
+  theServo.write(90 + servoTurnAngle);
+}
+
+void homeServo(Servo &theServo) {
+  theServo.write(90);
+}
+
+void notify()
+{
+  //------DC Motor-------
+
+  if ( Ps3.event.button_down.l2 ) {
+    Serial.println("Started pressing the left trigger button");
+//    digitalWrite(ENABLE, HIGH);
+//    digitalWrite(DIRA, LOW); //reverse
+//    digitalWrite(DIRB, HIGH);
+  }
+
+  if ( Ps3.event.button_up.l2 ) {
+    Serial.println("Released the left trigger button");
+//    digitalWrite(ENABLE, LOW); // disable
+  }
+  if ( Ps3.event.button_down.r2 ) {
+    Serial.println("Started pressing the right trigger button");
+//    digitalWrite(ENABLE, HIGH); // enable on
+//    digitalWrite(DIRA, HIGH); //one way
+//    digitalWrite(DIRB, LOW);
+  }
+  if ( Ps3.event.button_up.r2 ) {
+    Serial.println("Released the right trigger button");
+//    digitalWrite(ENABLE, LOW); // disable
+  }
+  // -------Servos------
+  if ( Ps3.event.button_down.cross ) {
+    Serial.println("Started pressing the cross button");
+    clockwise(servo1);
+  }
+
+  if ( Ps3.event.button_up.cross ) {
+    Serial.println("Released the cross button");
+    homeServo(servo1);
+  }
+
+  if ( Ps3.event.button_down.circle ) {
+    Serial.println("Started pressing the circle button");
+    anticlockwise(servo1);
+  }
+  if ( Ps3.event.button_up.circle ) {
+    Serial.println("Released the circle button");
+    homeServo(servo1);
+  }
+
+  if ( Ps3.event.button_down.square ) {
+    Serial.println("Started pressing the square button");
+    anticlockwise(servo2);
+  }
+  if ( Ps3.event.button_up.square ) {
+    Serial.println("Released the square button");
+    homeServo(servo2);
+  }
+  if ( Ps3.event.button_down.triangle ) {
+    Serial.println("Started pressing the triangle button");
+    clockwise(servo2);
+  }
+  if ( Ps3.event.button_up.triangle ) {
+    Serial.println("Released the triangle button");
+    homeServo(servo2);
+  }
+  if ( Ps3.event.button_down.up ) {
+    Serial.println("Started pressing the up button");
+    clockwise(servo3);
+  }
+
+  if ( Ps3.event.button_up.up ) {
+    Serial.println("Released the up button");
+    homeServo(servo3);
+  }
+
+  if ( Ps3.event.button_down.right ) {
+    Serial.println("Started pressing the right button");
+    anticlockwise(servo3);
+  }
+  if ( Ps3.event.button_up.right ) {
+    Serial.println("Released the right button");
+    homeServo(servo3);
+  }
+  if ( Ps3.event.button_down.down ) {
+    Serial.println("Started pressing the down button");
+    clockwise(servo4);
+  }
+  if ( Ps3.event.button_up.down ) {
+    Serial.println("Released the down button");
+    homeServo(servo4);
+  }
+  if ( Ps3.event.button_down.left ) {
+    Serial.println("Started pressing the left button");
+    anticlockwise(servo4);
+  }
+  if ( Ps3.event.button_up.left ) {
+    Serial.println("Released the left button");
+    homeServo(servo4);
+  }
+  if ( Ps3.event.button_down.r1 ) {
+    Serial.println("Started pressing R1");
+    moveright();
+  }
+  if ( Ps3.event.button_down.l1 ) {
+    Serial.println("Started pressing the L1");
+    moveleft();
+  }
+}
+
+void onConnect() {
+  Serial.println("Connected.");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Ps3.attach(notify);
+  Ps3.attachOnConnect(onConnect);
+  Ps3.begin("0c:ee:e6:4e:fb:db");
+  Serial.println("Ready.");
+
+//   sets the pins as outputs:
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(enable1Pin, OUTPUT);
+
+  // configure LED PWM functionalitites
+  ledcSetup(pwmChannel, freq, resolution);
+
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(enable1Pin, pwmChannel);
+  ledcAttachPin(ENABLE, pwmChannel);
+
+  servo1.attach(21);
+  servo2.attach(22);
+  servo3.attach(19);
+  servo4.attach(23);
+
+  pinMode(ENABLE, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+}
+
+void loop()
+{
+  if (!Ps3.isConnected())
+    return;
+  delay(2000);
+}
+
+
+void moveleft() {
+  digitalWrite(ENABLE, HIGH); // enable on
+  digitalWrite(DIRA, HIGH); //one way
+  digitalWrite(DIRB, LOW);
+  delay(700);
+  digitalWrite(ENABLE, LOW); // disable
+}
+
+void moveright() {
+  digitalWrite(ENABLE, HIGH);
+  digitalWrite(DIRA, LOW); //reverse
+  digitalWrite(DIRB, HIGH);
+  delay(700);
+  digitalWrite(ENABLE, LOW); // disable
+}
